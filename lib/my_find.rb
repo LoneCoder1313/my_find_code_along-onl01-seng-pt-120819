cc7b8d319a7d143b8873f851cf125fc3c4a74391
Ricky dildy <rdildy92@gmail.com>
2019-12-22 23:34:20 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,9 +2,10 @@ require 'pry'
 
 def my_find(collection)
   i = 0 
-  while i < 100
-  yield(1)
-  binding.pry
-  i = i + 1 
+  while i < collection.length 
+    if yield(collection[i])
+      return collection[i]
+    end 
+     i = i + 1 
 end 
 end
